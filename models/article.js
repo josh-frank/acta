cabf11dd0a53c3e2a740609dfad028991ecebeb5
Josh Frank <eurosewusa@gmail.com>
2021-04-27 16:27:35 -0400

    added db:reset script and article-edit relationships

--- models/article.js
+++ models/article.js
@@ -1,23 +1,23 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+"use strict";
+
+const { Model } = require( "sequelize" );
+
+module.exports = ( sequelize, DataTypes ) => {
   class Article extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate( models ) {
+      Article.hasMany( models.Edit, { foreignKey: "articleId", as: "articles" } );
     }
   };
-  Article.init({
+  Article.init( {
     title: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Article',
-  });
+    modelName: "Article",
+  } );
   return Article;
 };
--- models/edit.js
+++ models/edit.js
@@ -1,24 +1,24 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+"use strict";
+
+const { Model } = require( "sequelize" );
+
+module.exports = ( sequelize, DataTypes ) => {
   class Edit extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate( models ) {
+      Edit.belongsTo( models.Article, { foreignKey: "articleId", onDelete: "CASCADE" } );
     }
   };
-  Edit.init({
+  Edit.init( {
     content: DataTypes.STRING,
     current: DataTypes.BOOLEAN
   }, {
     sequelize,
-    modelName: 'Edit',
-  });
+    modelName: "Edit",
+  } );
   return Edit;
 };
