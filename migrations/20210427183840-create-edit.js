976e702090f0e2b24ddb362323b41fa6f5af95cf
Josh Frank <eurosewusa@gmail.com>
2021-04-27 14:44:25 -0400

    added models/migrations for article and edit

---
+++ migrations/20210427183840-create-edit.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Edits', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      current: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Edits');
+  }
+};
---
+++ seeders/20210427183131-sample-articles.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async ( queryInterface, Sequelize ) => {
+    return await queryInterface.bulkInsert( "Articles", [
+      { title: "Test1", createdAt: new Date(), updatedAt: new Date() },
+      { title: "Test2", createdAt: new Date(), updatedAt: new Date() },
+      { title: "Test3", createdAt: new Date(), updatedAt: new Date() },
+      { title: "Test4", createdAt: new Date(), updatedAt: new Date() },
+      { title: "Test5", createdAt: new Date(), updatedAt: new Date() },
+    ], {} );
+  },
+  down: async ( queryInterface, Sequelize ) => {
+    return await queryInterface.bulkDelete( "Articles", null, {} );
+  }
+};
